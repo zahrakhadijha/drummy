237aea4d9e0565a067a89cf42b9ee9275ebe00dd
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-20 20:56:23 -0500

    add functionality for all keys

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
+const keys = document.querySelectorAll('.key')
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition) )
+
+
+
 window.addEventListener('keydown', (e) => {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 	if (!audio) return // stop the function from running
 	audio.currentTime = 0 // rewinds to the start
-	audio.play()
+    audio.play()
+
+    key.classList.add('playing')
 })
