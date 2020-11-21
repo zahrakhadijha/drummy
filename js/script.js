63d4a3e203cec912a855f158fb04c68b4937162f
zahrakhadijha <zahra.khan1@icloud.com>
2020-11-20 21:00:48 -0500

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 const keys = document.querySelectorAll('.key')
 
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return
-    this.classList.remove('playing')
+	if (e.propertyName !== 'transform') return
+	this.classList.remove('playing')
 }
 
 
-keys.forEach(key => key.addEventListener('transitionend', removeTransition) )
-
-
-
-window.addEventListener('keydown', (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 	if (!audio) return // stop the function from running
 	audio.currentTime = 0 // rewinds to the start
-    audio.play()
+	audio.play()
+	key.classList.add('playing')
+}
 
-    key.classList.add('playing')
-})
+window.addEventListener('keydown', playSound)
